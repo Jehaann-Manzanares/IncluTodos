5e37f690ea34e6913f1d9ae9b515cc0036a628e0
Jehaann Manzanares <jehaann07carlos@gmail.com>
2022-11-25 21:49:33 -0600

    (feature):adding hero component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from './Components/Home'
 import  "./fonts/FontsFree-Net-Circular-Std-Medium.ttf"
+import  "./fonts/FontsFree-Net-Jeko-Extra-Bold.ttf"
 
 function App() {
   return (
