89b89a04fd918dc69de84c8c70f39f37cbed9af0
Jehaann Manzanares <jehaann07carlos@gmail.com>
2022-11-26 08:34:47 -0600

    (feature) responsive tablet and desktops

--- src/Helpers/wrapper.js
+++ src/Helpers/wrapper.js
@@ -2,7 +2,7 @@ import React, { Children } from "react";
 import styled from "styled-components";
 
 const WrapperStyled = styled.div`
-    
+    width:100vw;
     max-width: 1400px;
     margin: auto;
     padding: 0 1rem;
